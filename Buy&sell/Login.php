6309074b4735328ccbc2c3152b159a09891cb152
Mukesh Bangar <205112042@nitt.edu>
2013-10-15 18:30:28 +0530

    MUKESH BANGAR 			PROJECT ON 			BUY AND SELL

---
+++ Buy&sell/Login.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+body {
+	background-image: url(b&s.jpg);
+}
+.style1 {
+	color: #FF0000;
+	font-weight: bold;
+}
+.style2 {color: #FF0000}
+-->
+</style>
+<link href="../../../Users/Bangar/Documents/Unnamed Site 1/forms.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+.style3 {font-family: Arial, Helvetica, sans-serif}
+.style4 {color: #000000}
+.style5 {font-family: "Times New Roman", Times, serif}
+-->
+</style>
+</head>
+
+<body>
+<div align="justify"></div>
+<form id="form3" name="form3" method="post" action="">
+  <label>
+  <div align="justify">
+    <blockquote>
+      <p>
+        <input name="Buy&amp;sell" type="reset" class="style1" id="Buy&amp;sell" value="Buy&amp;sell" />
+      </p>
+    </blockquote>
+  </div>
+  </label>
+</form>
+<div align="justify">
+  <blockquote>&nbsp;</blockquote>
+  <table width="200" border="0" align="right" bordercolor="#000000">
+    <tr>
+      <td><span class="style1 style5">Username</span></td>
+      <td><form action="" method="post" name="form1" class="style3" id="form1">
+        <label>
+          <input name="username" type="text" id="username" />
+          </label>
+      </form>    </td>
+      <td><span class="style2 style5">Password</span></td>
+      <td><form action="" method="post" name="form2" class="style3" id="form2">
+        <label>
+          <input name="password" type="password" id="password" maxlength="8" />
+          </label>
+      </form>    </td>
+    </tr>
+    <tr>
+      <td colspan="4"><span class="style3"></span><span class="style3"></span><span class="style3"></span></td>
+    </tr>
+    <tr>
+      <td colspan="4">&nbsp;</td>
+    </tr>
+    <tr>
+      <td colspan="2"><div align="justify"><span class="style3"></span><span class="style5">
+        <input name="checkbox" type="checkbox" class="style2" value="checkbox" />
+      <span class="style4">Stay Login </span></span><span class="style3"></span><span class="style3"></span></div></td>
+      <td colspan="2">
+        <div align="justify"><strong>
+          <input name="Login" type="submit" id="Login" value="Login" />
+      </strong></div></td>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
---
+++ Buy&sell/README.txt
@@ -0,0 +1,17 @@
+			Buy&Sell
+
+
+This application will help in selling old things online.
+Students can purchase and sell the things on internet.
+It will save time and also students can have things on less price.
+
+Advantages:
+
+It will save time.
+Old things will not waste.
+Students easily can sell and purchase old things.
+
+Note:
+
+This is the starting page of the project.
+Now i have created log in page.
